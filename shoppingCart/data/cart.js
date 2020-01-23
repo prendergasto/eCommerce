1affbfc175039e6238e4c0f478aebe8c654748c9
Owen Prendergast <prendergasto@gmail.com>
2020-01-23 09:59:24 -0800

    cart data

---
+++ shoppingCart/data/cart.js
@@ -0,0 +1,15 @@
+const cart = [{
+    id: 'ferrari',
+    quantity: 1
+}, {
+    id: 'toyota',
+    quantity: 2
+}, {
+    id: 'kia',
+    quantity: 1
+}, {
+    id: 'bentley',
+    quantity: 3
+}];
+
+export default cart;
